054708fccc25b805687ec60a6f4c6113aba077cb
Aditya Gusain <adisandi3112002@gmail.com>
2021-11-17 19:33:42 +0530

    Updated the folder structure

--- server/database/connection.js
+++ server/database/connection.js
@@ -4,7 +4,5 @@ export default async () => {
   return mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
   });
 };

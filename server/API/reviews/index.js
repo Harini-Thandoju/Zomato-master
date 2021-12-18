b488e4fd6480e14349821645d2209d1698470e2d
Aditya Gusain <adisandi3112002@gmail.com>
2021-12-18 21:01:37 +0530

    Day 34 - Auth reducer and added signin, signup and logout functionality

--- server/API/reviews/index.js
+++ server/API/reviews/index.js
@@ -1,5 +1,6 @@
 // Libraries
 import express from "express";
+import passport from "passport";
 
 // Database Model
 import { ReviewModel } from "../../database/allModels";
@@ -28,14 +29,15 @@ Router.get("/:resid", async (req, res) => {
  * Route        /new
  * Des          POST: Adding new food/restaurant review and rating
  * Params       none
- * Access       Public
+ * Access       Private
  * Method       POST
  */
-Router.post("/new", async (req, res) => {
+Router.post("/new", passport.authenticate("jwt"), async (req, res) => {
   try {
+    const { _id } = req.session.passport.user._doc;
     const { reviewData } = req.body;
 
-    await ReviewModel.create({ ...reviewData });
+    await ReviewModel.create({ ...reviewData, user: _id });
 
     return res.json({ reviews: "Successfully Created Review" });
   } catch (error) {
